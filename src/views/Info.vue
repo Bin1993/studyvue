47105c8094701b517a91a55634757291dc973cfe
wbb1993 <wbb1993@126.com>
2019-01-18 17:20:00 +0800

    first

---
+++ src/views/Info.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        hello info component
+        <button type="button" @click="add()">添加</button>
+    </div>
+</template>
+
+<script>
+    import store from '@/store'
+    export default {
+        name: "Info",
+        store,
+        methods:{
+            add(){
+                console.log('add event'),
+                store.commit('increase')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
